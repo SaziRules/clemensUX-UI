37cdece0a59d0e90d77f43a2d5ac072ba10f1b06
SaziRules <sazi@thepitchdot.co.za>
2025-01-24 11:29:02 +0200

    Cemens Unisex Pad Data

---
+++ app/stores/page.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Stores = () => {
+  return (
+    <View>
+      <Text>Where to buy</Text>
+    </View>
+  )
+}
+
+export default Stores
